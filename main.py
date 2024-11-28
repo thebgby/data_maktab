095abad335ecb10f5c37a3cc12f76b82bb289039
Roma Ubuntu <haskerboe@gmail.com>
2024-11-28 10:46:28 +0500

    just one change in main file

--- main.py
+++ main.py
@@ -53,82 +53,82 @@ from logs import logger
 #         logger.error(f"error while loading [maktab kassa] data: {e}")
 
 # stipendiya data etl
-# stipendiya_data = None
-# try:
-#     stipendiya_data = stipendiya_extract(stipendiya)
-#     logger.info(f"[stipendiya] data successfully extracted")
-# except Exception as e:
-#     logger.error(f"error while extracting [stipendiya] data: {e}")
-#
-# if not stipendiya_data.empty:
-#     try:
-#         stipendiya_load(stipendiya_data)
-#         logger.info(f"[stipendiya] data successfully loaded")
-#     except Exception as e:
-#         logger.error(f"error while loading [stipendiya] data: {e}")
-#
-# # maktab_xodimlar data etl
-# m_xodimlar_data = None
-# try:
-#     m_xodimlar_data = maktab_xodimlar_extract(maktab_xodimlar)
-#     logger.info(f"[maktab xodimlar] data successfully extracted")
-# except Exception as e:
-#     logger.error(f"error while extracting [maktab xodimlar] data: {e}")
-#
-# if  m_xodimlar_data:
-#     try:
-#         maktab_xodimlar_load(m_xodimlar_data)
-#         logger.info(f"[maktab xodimlar] data successfully loaded")
-#     except Exception as e:
-#         logger.error(f"error while loading [maktab xodimlar] data: {e}")
-#
-#
-# # maktab_xodimlar data etl
-# pochemuchkadb_data = None
-# try:
-#     pochemuchkadb_data = pochemuchkadb_extract(pochemuchkadb)
-#     logger.info(f"[pochemuchkadb] data successfully extracted")
-# except Exception as e:
-#     logger.error(f"error while extracting [pochemuchkadb] data: {e}")
-#
-# if  pochemuchkadb_data:
-#     try:
-#         pochemuchkadb_load(pochemuchkadb_data)
-#         logger.info(f"[pochemuchkadb] data successfully loaded")
-#     except Exception as e:
-#         logger.error(f"error while loading [pochemuchkadb] data: {e}")
-#
-#
-# # dars jadvali data etl
-# darsjadval_data = None
-# try:
-#     darsjadval_data = darsjadvali_extract(dars_jadvali)
-#     logger.info(f"[dars jadvali] data successfully extracted")
-# except Exception as e:
-#     logger.error(f"error while extracting [dars jadvali] data: {e}")
-#
-# if  not darsjadval_data.empty:
-#     try:
-#         darsjadvali_load(darsjadval_data)
-#         logger.info(f"[dars jadvali] data successfully loaded")
-#     except Exception as e:
-#         logger.error(f"error while loading [dars jadvali] data: {e}")
+stipendiya_data = None
+try:
+    stipendiya_data = stipendiya_extract(stipendiya)
+    logger.info(f"[stipendiya] data successfully extracted")
+except Exception as e:
+    logger.error(f"error while extracting [stipendiya] data: {e}")
 
-# # dars jadvali data etl
-# psixolog_test_data = None
-# try:
-#     psixolog_test_data = psixolog_test_extract(psixolog_test)
-#     # print(type(psixolog_test_data))
-#     logger.info(f"[psixolog test] data successfully extracted")
-# except Exception as e:
-#     logger.error(f"error while extracting [psixolog test] data: {e}")
-#
-# if  psixolog_test_data:
-#     try:
-#         psixolog_test_load(psixolog_test_data)
-#         logger.info(f"[psixolog test] data successfully loaded")
-#     except Exception as e:
-#         logger.error(f"error while loading [psixolog test] data: {e}")
+if not stipendiya_data.empty:
+    try:
+        stipendiya_load(stipendiya_data)
+        logger.info(f"[stipendiya] data successfully loaded")
+    except Exception as e:
+        logger.error(f"error while loading [stipendiya] data: {e}")
+
+# maktab_xodimlar data etl
+m_xodimlar_data = None
+try:
+    m_xodimlar_data = maktab_xodimlar_extract(maktab_xodimlar)
+    logger.info(f"[maktab xodimlar] data successfully extracted")
+except Exception as e:
+    logger.error(f"error while extracting [maktab xodimlar] data: {e}")
+
+if  m_xodimlar_data:
+    try:
+        maktab_xodimlar_load(m_xodimlar_data)
+        logger.info(f"[maktab xodimlar] data successfully loaded")
+    except Exception as e:
+        logger.error(f"error while loading [maktab xodimlar] data: {e}")
+
+
+# maktab_xodimlar data etl
+pochemuchkadb_data = None
+try:
+    pochemuchkadb_data = pochemuchkadb_extract(pochemuchkadb)
+    logger.info(f"[pochemuchkadb] data successfully extracted")
+except Exception as e:
+    logger.error(f"error while extracting [pochemuchkadb] data: {e}")
+
+if  pochemuchkadb_data:
+    try:
+        pochemuchkadb_load(pochemuchkadb_data)
+        logger.info(f"[pochemuchkadb] data successfully loaded")
+    except Exception as e:
+        logger.error(f"error while loading [pochemuchkadb] data: {e}")
+
+
+# dars jadvali data etl
+darsjadval_data = None
+try:
+    darsjadval_data = darsjadvali_extract(dars_jadvali)
+    logger.info(f"[dars jadvali] data successfully extracted")
+except Exception as e:
+    logger.error(f"error while extracting [dars jadvali] data: {e}")
+
+if  not darsjadval_data.empty:
+    try:
+        darsjadvali_load(darsjadval_data)
+        logger.info(f"[dars jadvali] data successfully loaded")
+    except Exception as e:
+        logger.error(f"error while loading [dars jadvali] data: {e}")
+
+# dars jadvali data etl
+psixolog_test_data = None
+try:
+    psixolog_test_data = psixolog_test_extract(psixolog_test)
+    # print(type(psixolog_test_data))
+    logger.info(f"[psixolog test] data successfully extracted")
+except Exception as e:
+    logger.error(f"error while extracting [psixolog test] data: {e}")
+
+if  psixolog_test_data:
+    try:
+        psixolog_test_load(psixolog_test_data)
+        logger.info(f"[psixolog test] data successfully loaded")
+    except Exception as e:
+        logger.error(f"error while loading [psixolog test] data: {e}")
 
 # kutubxona data etl
 kutubxona_data = None
